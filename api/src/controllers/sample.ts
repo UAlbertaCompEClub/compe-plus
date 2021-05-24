dc02339200c3f2be206292e41e638074a5ce012b
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-24 15:19:26 -0600

    Revert "Test ci cache-hit"
    This reverts commit e46f8b195fb8b1d47e6fbbaa741524e246418f74.

--- api/src/controllers/sample.ts
+++ api/src/controllers/sample.ts
@@ -5,7 +5,6 @@ const NAMESPACE = 'Sample Controller';
 
 const sampleHealthCheck = (req: Request, res: Response): Response => {
   logging.info(NAMESPACE, 'Sample health check route called.');
-  logging.info(NAMESPACE, 'test github actions no package-lock.json change');
 
   return res.status(200).json({ message: 'pong' });
 };
