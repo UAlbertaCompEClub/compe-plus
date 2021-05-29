8527e2e29ed7ab92fe5803d717dcb95771c9edc5
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-05-29 15:49:54 -0600

    Scaffold db (#20)
    * Write the api README.md
    
    * Add README.md for DB
    
    * Update REAMDE for DB
    
    * Rough iteration on initial migration
    
    * Comment out schema
    
    * Write the api README.md
    
    * Rough iteration on initial migration
    
    * update tech stack

---
+++ api/db/README.md
@@ -0,0 +1,52 @@
+# PostgreSQL
+
+We use PostgreSQL 13.x to store data for CompE+.
+
+# Local Development
+
+To run a Postgres instance locally we leverage Docker. The following instructions are based on [this](https://hackernoon.com/dont-install-postgres-docker-pull-postgres-bee20e200198).
+
+## Setup
+
+1. [Install Docker](https://www.docker.com/get-started)
+2. Pull down the Postgres Docker image `docker pull postgres:13`
+3. Create a directory to serve as the local host mount point for Postgres data files `mkdir -p $HOME/docker/volumes/postgres`
+4. Install `psql` via `sudo apt-get install postgresql-client`
+
+## Starting Postgres
+
+`docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=compe-plus -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:13`
+
+Verify that it is running with `docker ps` or kill it with `docker kill pg-docker`
+
+## Connecting to Postgres
+
+`psql -h localhost -U postgres -d compe-plus` (Password is "postgres")
+
+# Production
+
+In production a Postgres instance is provisioned via a Heroku add-on.
+
+# Migrations
+
+To manage migrations within Postgres we use [golang-migrate](https://github.com/golang-migrate/migrate). Look [here](https://github.com/golang-migrate/migrate/blob/master/database/postgres/TUTORIAL.md) for a decent high-level tutorial of the process.
+
+## Setup
+
+```bash
+# Install the tool
+cd ~/Downloads
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
+mv migrate.linux-amd64.tar.gz /usr/local/bin/migrate
+
+# Make sure it works
+migrate --version
+```
+
+## Creating a new migration
+
+`migrate create -ext sql -seq -digits 3 -dir db/migrations/ <NAME>`
+
+## Applying a migration locally
+
+`migrate -database 'postgres://postgres:postgres@localhost:5432/compe-plus?sslmode=disable' -path db/migrations up`
---
+++ api/db/migrations/001_initial_schema.up.sql
@@ -0,0 +1,51 @@
+-- TODO
+-- -- TODO varchar lengths
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- CREATE TABLE IF NOT EXISTS users (
+--   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+--   email VARCHAR (300) UNIQUE NOT NULL,
+--   ccid VARCHAR (300) NOT NULL,
+--   year INTEGER,
+--   degree VARCHAR (300) NOT NULL,
+--   -- Username / actual name from Oauth
+--   -- Access token / session
+--   -- Additional contact information
+--   -- Roles?
+--   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+-- );
+-- CREATE TABLE IF NOT EXISTS resumes (
+--   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+--   -- TODO cascade or restrict?
+--   user_id UUID REFERENCES users ON DELETE RESTRICT,
+--   url TEXT,
+--   -- Reviewing state? Enum? Created, reviewing, archived?
+--   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+-- );
+-- CREATE TABLE IF NOT EXISTS reviews (
+--   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+--   -- TODO cascade or restrict?
+--   user_id UUID REFERENCES users ON DELETE RESTRICT,
+--   resume_id UUID REFERENCES resumes ON DELETE CASCADE,
+--   url TEXT,
+--   -- Reviewing state? Enum?
+--   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+-- );
+-- CREATE
+-- OR REPLACE FUNCTION update_modified_column() RETURNS TRIGGER AS $ func $ BEGIN NEW.updated_at = NOW();
+-- RETURN NEW;
+-- END;
+-- $ func $ LANGUAGE 'plpgsql';
+-- CREATE TRIGGER update_time_users BEFORE
+-- UPDATE
+--   ON users FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+-- CREATE TRIGGER update_time_resumes BEFORE
+-- UPDATE
+--   ON resumes FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+-- CREATE TRIGGER update_time_reviews BEFORE
+-- UPDATE
+--   ON reviews FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+-- -- Table for each resume reviewer, interviewer
+-- -- Table for feedback?
