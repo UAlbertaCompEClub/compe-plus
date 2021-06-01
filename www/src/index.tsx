40033691c42b0c67ad7d551d3c92d55aa72e3697
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-31 20:28:17 -0600

    Integrate AuthProvider

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Auth0ProviderWithHistory from './providers/Auth0ProviderWithHistory';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Auth0ProviderWithHistory>
+                <App />
+            </Auth0ProviderWithHistory>
+        </Router>
     </React.StrictMode>,
     document.getElementById('root'),
 );
