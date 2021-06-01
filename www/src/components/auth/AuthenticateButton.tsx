9a14ef43c08d06bdc6506d0f9fe7da6d2f107d81
Armianto Sumitro <sumitro@ualberta.ca>
2021-05-31 20:28:23 -0600

    Add authenticate button

---
+++ www/src/components/auth/AuthenticateButton.tsx
@@ -0,0 +1,39 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+type BaseButtonProps = {
+    text: string;
+    onClick: () => void;
+};
+
+const BaseButton: React.FC<BaseButtonProps> = (props) => {
+    return <button onClick={(_e) => props.onClick()}>{props.text}</button>;
+};
+
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+    return <BaseButton text="Logout" onClick={() => logout()} />;
+};
+
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+    return <BaseButton text="Login" onClick={() => loginWithRedirect()} />;
+};
+
+const AuthenticateButton: React.FC = () => {
+    const { isAuthenticated } = useAuth0();
+
+    if (isAuthenticated) {
+        return <LogoutButton />;
+    }
+
+    return <LoginButton />;
+};
+
+BaseButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+};
+
+export default AuthenticateButton;
