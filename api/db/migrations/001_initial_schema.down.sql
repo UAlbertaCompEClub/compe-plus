cb26f180a1094229f7f6ad2f45d0d43c58336685
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-05-31 20:56:40 -0600

    Design 1st draft of DB schema (#32)
    * Rough outline of REST API endpoints
    
    * Translate diagram into schema
    
    * Update docs on postgres and add shortcut command for local db work
    
    * Add photo of schema
    
    * Rename fields
    
    * Add ON DELETE clauses
    
    * Update schema photo

--- api/db/migrations/001_initial_schema.down.sql
+++ api/db/migrations/001_initial_schema.down.sql
@@ -1,1 +1,31 @@
--- TODO
+DROP TRIGGER IF EXISTS update_time_interviews ON interviews;
+
+DROP TABLE IF EXISTS interviews;
+
+DROP TRIGGER IF EXISTS update_time_time_slots ON time_slots;
+
+DROP TABLE IF EXISTS time_slots;
+
+DROP TRIGGER IF EXISTS update_time_documents ON documents;
+
+DROP TABLE IF EXISTS documents;
+
+DROP TABLE IF EXISTS document_states;
+
+DROP TRIGGER IF EXISTS update_time_resume_reviews ON resume_reviews;
+
+DROP TABLE IF EXISTS resume_reviews;
+
+DROP TABLE IF EXISTS resume_review_states;
+
+DROP TABLE IF EXISTS user_roles;
+
+DROP TABLE IF EXISTS roles;
+
+DROP TRIGGER IF EXISTS update_time_users ON users;
+
+DROP TABLE IF EXISTS users;
+
+DROP FUNCTION IF EXISTS update_modified_column();
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
--- api/db/migrations/001_initial_schema.up.sql
+++ api/db/migrations/001_initial_schema.up.sql
@@ -1,51 +1,137 @@
--- TODO
--- -- TODO varchar lengths
--- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- CREATE TABLE IF NOT EXISTS users (
---   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
---   email VARCHAR (300) UNIQUE NOT NULL,
---   ccid VARCHAR (300) NOT NULL,
---   year INTEGER,
---   degree VARCHAR (300) NOT NULL,
---   -- Username / actual name from Oauth
---   -- Access token / session
---   -- Additional contact information
---   -- Roles?
---   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
---   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
--- );
--- CREATE TABLE IF NOT EXISTS resumes (
---   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
---   -- TODO cascade or restrict?
---   user_id UUID REFERENCES users ON DELETE RESTRICT,
---   url TEXT,
---   -- Reviewing state? Enum? Created, reviewing, archived?
---   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
---   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
--- );
--- CREATE TABLE IF NOT EXISTS reviews (
---   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
---   -- TODO cascade or restrict?
---   user_id UUID REFERENCES users ON DELETE RESTRICT,
---   resume_id UUID REFERENCES resumes ON DELETE CASCADE,
---   url TEXT,
---   -- Reviewing state? Enum?
---   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
---   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
--- );
--- CREATE
--- OR REPLACE FUNCTION update_modified_column() RETURNS TRIGGER AS $ func $ BEGIN NEW.updated_at = NOW();
--- RETURN NEW;
--- END;
--- $ func $ LANGUAGE 'plpgsql';
--- CREATE TRIGGER update_time_users BEFORE
--- UPDATE
---   ON users FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
--- CREATE TRIGGER update_time_resumes BEFORE
--- UPDATE
---   ON resumes FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
--- CREATE TRIGGER update_time_reviews BEFORE
--- UPDATE
---   ON reviews FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
--- -- Table for each resume reviewer, interviewer
--- -- Table for feedback?
+/* Enable UUID support */
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+/* Auto timestamping function */
+CREATE OR REPLACE FUNCTION update_modified_column()
+    RETURNS TRIGGER AS
+$func$
+BEGIN
+    NEW.updated_at = now();
+    return NEW;
+END;
+$func$ LANGUAGE 'plpgsql';
+
+/* users table */
+CREATE TABLE IF NOT EXISTS users (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email TEXT NOT NULL,
+    ccid TEXT NOT NULL,
+    program TEXT NOT NULL,
+    year SMALLINT NOT NULL,
+    given_name TEXT NOT NULL,
+    family_name TEXT NOT NULL,
+    full_name TEXT NOT NULL,
+    photo_url TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER update_time_users
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+/* roles table */
+CREATE TABLE roles (
+    name text NOT NULL PRIMARY KEY
+);
+
+INSERT INTO
+    roles (name)
+VALUES
+    ('reviewer'),
+    ('interviewer'),
+    ('admin');
+
+/* user_roles table */
+CREATE TABLE user_roles (
+    user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE,
+    role TEXT NOT NULL REFERENCES roles ON DELETE CASCADE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (user_id, role)
+);
+
+/* resume_review_states table */
+CREATE TABLE resume_review_states (
+    name TEXT NOT NULL PRIMARY KEY
+);
+
+INSERT INTO
+    resume_review_states (name)
+VALUES
+    ('creating'),
+    ('seeking_reviewer'),
+    ('reviewing'),
+    ('finished'),
+    ('cancelled');
+
+/* resume_reviews table */
+CREATE TABLE resume_reviews (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    reviewee_id UUID NOT NULL REFERENCES users ON DELETE RESTRICT,
+    reviewer_id UUID REFERENCES users ON DELETE RESTRICT,
+    state TEXT NOT NULL REFERENCES resume_review_states ON DELETE RESTRICT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER update_time_resume_reviews
+    BEFORE UPDATE
+    ON resume_reviews
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+/* document_states table */
+CREATE TABLE document_states (
+    name TEXT NOT NULL PRIMARY KEY
+);
+
+INSERT INTO
+    document_states (name)
+VALUES
+    ('creating'),
+    ('ready');
+
+/* documents table */
+CREATE TABLE documents (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    note TEXT NOT NULL,
+    file_url TEXT NOT NULL,
+    is_review BOOLEAN NOT NULL,
+    user_id UUID NOT NULL REFERENCES users ON DELETE RESTRICT,
+    state TEXT NOT NULL REFERENCES document_states ON DELETE RESTRICT,
+    resume_review_id UUID NOT NULL REFERENCES resume_reviews ON DELETE RESTRICT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER update_time_documents
+    BEFORE UPDATE
+    ON documents
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+/* time_slots table */
+CREATE TABLE time_slots (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    at TIMESTAMPTZ NOT NULL,
+    user_id UUID NOT NULL REFERENCES users ON DELETE RESTRICT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER update_time_time_slots
+    BEFORE UPDATE
+    ON time_slots
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+/* interviews table */
+CREATE TABLE interviews (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    time_slot UUID NOT NULL REFERENCES time_slots ON DELETE RESTRICT,
+    interviewee UUID NOT NULL REFERENCES users ON DELETE RESTRICT,
+    interviewer UUID NOT NULL REFERENCES users ON DELETE RESTRICT
+);
+
+CREATE TRIGGER update_time_interviews
+    BEFORE UPDATE
+    ON interviews
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
