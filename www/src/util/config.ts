1e4ca83963ec24c76e31592f1d776b4189eadba3
Armianto Sumitro <armiantos@gmail.com>
2021-06-06 15:59:15 -0600

    Set-up environment variables for server in client

--- www/src/util/config.ts
+++ www/src/util/config.ts
@@ -1,13 +1,22 @@
 const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || '';
 const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
 
+const SERVER_ENDPOINT = process.env.SERVER_ENDPOINT || '';
+const SERVER_AUDIENCE = process.env.SERVER_AUDIENCE || '';
+
 const AUTH0 = {
     domain: AUTH0_DOMAIN,
     clientId: AUTH0_CLIENT_ID,
 };
 
+const SERVER = {
+    endpoint: SERVER_ENDPOINT,
+    audience: SERVER_AUDIENCE,
+};
+
 const config = {
     auth0: AUTH0,
+    server: SERVER,
 };
 
 export default config;
