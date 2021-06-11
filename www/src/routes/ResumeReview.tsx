55962db9d0d366118a92445a76fcef9978804b4d
Armianto Sumitro <armiantos@gmail.com>
2021-06-10 20:41:39 -0600

    Move ping example to resume review

--- www/src/routes/ResumeReview.tsx
+++ www/src/routes/ResumeReview.tsx
@@ -1,8 +1,29 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import React, { FC } from 'react';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
+import config from '../util/config';
 
 const ResumeReview: FC = () => {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+    try {
+        async () => {
+            const token = await getAccessTokenSilently({
+                audience: config.server.audience,
+                scope: 'call:ping',
+            });
+
+            const response = await axios.get(`${config.server.endpoint}/api/secure/v1/ping`, {
+                headers: {
+                    authorization: `Bearer ${token}`,
+                },
+            });
+
+            console.log(response);
+        };
+    } catch (e) {
+        console.error(e);
+    }
 
     return isAuthenticated ? <div>ResumeReview (logged in!!)</div> : <div>ResumeReview (not logged in!!)</div>;
 };
