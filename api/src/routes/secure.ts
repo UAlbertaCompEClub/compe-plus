d2aeed39c4119fa76e59823345c2001ca8e2753c
Armianto Sumitro <armiantos@gmail.com>
2021-06-13 13:24:57 -0600

    Rename jwtAuthz to authorize

--- api/src/routes/secure.ts
+++ api/src/routes/secure.ts
@@ -2,12 +2,12 @@ import express from 'express';
 
 import middleware from './middleware';
 import controller from '../controllers/sample';
-import jwtAuthz from 'express-jwt-authz';
+import authorize from 'express-jwt-authz';
 
 const router = express.Router();
 
 router.use(middleware.authenticate());
 
-router.get('/ping', jwtAuthz(['call:ping']), controller.sampleHealthCheck);
+router.get('/ping', authorize(['call:ping']), controller.sampleHealthCheck);
 
 export = router;
