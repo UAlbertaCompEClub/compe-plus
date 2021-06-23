697799b3b809168b73da75c405d2f021e935a206
Cyrus Diego <cyrus.d04@gmail.com>
2021-06-23 14:43:37 -0600

    fixed lint

--- www/src/hooks/useVisibility.ts
+++ www/src/hooks/useVisibility.ts
@@ -1,6 +1,6 @@
 // Credit: https://stackoverflow.com/questions/59595700/how-to-make-a-react-component-fade-in-on-scroll-using-intersectionobserver-but
 
-import React, { useState, useEffect, RefObject } from 'react';
+import { useState, useEffect, RefObject } from 'react';
 
 export default function useVisibility(ref: RefObject<HTMLDivElement>): boolean {
     const [isVisible, setVisibility] = useState(false);
