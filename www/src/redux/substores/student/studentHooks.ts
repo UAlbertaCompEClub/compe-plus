8a723f72769dbd90da28f98db2b70c5d0a6ab35a
Armianto Sumitro <armiantos@gmail.com>
2021-06-26 14:14:12 -0600

    Strongly type dispatches

--- www/src/redux/substores/student/studentHooks.ts
+++ www/src/redux/substores/student/studentHooks.ts
@@ -2,6 +2,5 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { StudentDispatch, StudentState } from './studentStore';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useStudentDispatch = () => useDispatch<StudentDispatch>();
+export const useStudentDispatch = (): StudentDispatch => useDispatch<StudentDispatch>();
 export const useStudentSelector: TypedUseSelectorHook<StudentState> = useSelector;
