918bb2d663f18d59213e39d09999a41411d83ff8
Anushka Khare <khareanushka2015@gmail.com>
2021-06-28 20:48:24 -0600

    Add placeholder toggle button

--- www/src/components/user/EditRolesDialog.tsx
+++ www/src/components/user/EditRolesDialog.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import React, { useState } from 'react';
 
 function EditRolesDialog() {
-    return <div>EditRolesDialog</div>;
+    const [isopen, setisopen] = useState(false);
+    // function openDialog() {
+    //     setisopen(true);
+    // }
+    return (
+        <>
+            <Dialog onClose={() => setisopen(false)} aria-labelledby='simple-dialog-title' open={isopen}>
+                <DialogTitle id='simple-dialog-title'>Edit Roles</DialogTitle>
+            </Dialog>
+            <Button variant='contained' onClick={(e) => setisopen(true)}>
+                Default{' '}
+            </Button>
+        </>
+    );
 }
 export default EditRolesDialog;
