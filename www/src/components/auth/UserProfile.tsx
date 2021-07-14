87bbffe83c29e9c8b2889824e0bb4400feff1ddb
rachel-ellis <rjellis@ualberta.ca>
2021-07-13 19:28:16 -0600

    display menu

--- www/src/components/auth/UserProfile.tsx
+++ www/src/components/auth/UserProfile.tsx
@@ -1,11 +1,30 @@
-import { IconButton } from '@material-ui/core';
+import { IconButton, Menu, MenuItem } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 
 export const UserProfile: FC = () => {
+    const userProfileIcon = useRef(null);
     return (
-        <IconButton aria-label='account of current user' aria-controls='menu-appbar' aria-haspopup='true' onClick={handleMenu} color='inherit'>
-            <AccountCircle />
-        </IconButton>
+        <>
+            <IconButton aria-label='account of current user' aria-controls='menu-appbar' aria-haspopup='true' color='inherit' ref={userProfileIcon}>
+                <AccountCircle />
+            </IconButton>
+            <Menu
+                id='menu-appbar'
+                anchorEl={userProfileIcon.current}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={true}
+            >
+                <MenuItem>Logout</MenuItem>
+            </Menu>
+        </>
     );
 };
