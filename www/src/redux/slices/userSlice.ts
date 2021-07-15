ee645a886641ecf24c4e29a0c857c2e5a789eb9b
Armianto Sumitro <armiantos@gmail.com>
2021-07-14 23:45:19 -0600

    Add user information to userSlice

--- www/src/redux/slices/userSlice.ts
+++ www/src/redux/slices/userSlice.ts
@@ -1,11 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+type UserStore = {
+    roles: string[];
+    currentRole: string | null;
+    info: UserInfo | null;
+};
+
+type UserInfo = {
+    id: string;
+    email: string;
+    ccid: string;
+    program: string;
+    year: number;
+    givenName: string;
+    familyName: string;
+    fullName: string;
+    photoUrl?: string;
+};
+
+const initialState: UserStore = {
+    roles: [],
+    currentRole: null,
+    info: null,
+};
+
 export const userSlice = createSlice({
     name: 'user',
-    initialState: {
-        roles: [],
-        currentRole: '',
-    },
+    initialState,
     reducers: {
         setCurrentRole(state, action: PayloadAction<string>) {
             state.currentRole = action.payload;
