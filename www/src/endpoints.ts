6b4a838839fb64e0a71ff6f57145d499cd8439c9
Armianto Sumitro <armiantos@gmail.com>
2021-07-15 00:32:31 -0600

    Fix users endpoint

--- www/src/endpoints.ts
+++ www/src/endpoints.ts
@@ -2,4 +2,4 @@ import config from './util/config';
 
 const apiPrefix = 'api/v1';
 
-export const users_endpoint = `${config.server.endpoint}/${apiPrefix}/user`;
+export const users_endpoint = `http://${config.server.endpoint}/${apiPrefix}/users`;
