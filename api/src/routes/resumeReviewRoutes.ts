4fad2d3b8641db16a694880e2a7ec683cf65e27a
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-07-16 00:03:55 -0600

    Stub out other endpoints in exisiting routers

--- api/src/routes/resumeReviewRoutes.ts
+++ api/src/routes/resumeReviewRoutes.ts
@@ -2,12 +2,21 @@ import express from 'express';
 
 import middleware from '../controllers/middleware';
 import * as controller from '../controllers/resumeReview/index';
+import NotImplementedException from '../exceptions/NotImplementedException';
 import Scope from '../types/scopes';
 
 export const router = express.Router();
 
 router.get('/resume-reviews', middleware.authorizeAndFallThrough(Scope.ReadAllResumeReviews), controller.getAllResumeReviews);
 router.get('/resume-reviews', middleware.authorize(Scope.ReadMyResumeReviews), controller.getMyResumeReviews);
+
 router.post('/resume-reviews', middleware.authorize(Scope.CreateResumeReviews), controller.postResumeReview);
+
+router.patch('/resume-reviews/:resumeReview', middleware.authorizeAndFallThrough(Scope.UpdateAllResumeReviews), () => {
+    throw new NotImplementedException('PATCH /resume-reviews/:resumeReview');
+});
+router.patch('/resume-reviews/:resumeReview', middleware.authorize(Scope.UpdateMyResumeReviews), () => {
+    throw new NotImplementedException('PATCH /resume-reviews/:resumeReview');
+});
 
 export default router;
--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -10,6 +10,7 @@ export const router = express.Router();
 router.get('/users', middleware.authorize(Scope.ReadUsers), () => {
     throw new NotImplementedException('GET /users');
 });
+
 router.post('/users', controller.postUser);
 
 export default router;
