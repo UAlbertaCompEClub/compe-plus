5c901d09c8c85c2bcd7006bf13a7b92aaacc4200
Armianto Sumitro <armiantos@gmail.com>
2021-07-20 21:27:31 -0600

    Add happy path tests

--- api/src/controllers/user/getUser.test.ts
+++ api/src/controllers/user/getUser.test.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { mocked } from 'ts-jest/utils';
 
+import NotAuthorizedException from '../../exceptions/NotAuthorizedException';
 import * as userRepository from '../../repositories/userRepository';
 import testConstants from '../../util/testConstants';
 import getUser from './getUser';
@@ -25,12 +26,13 @@ beforeEach(() => {
             sub: testConstants.user1.id,
         },
     };
+    res = { status: jest.fn().mockReturnThis(), json: jest.fn().mockReturnThis() };
     next = jest.fn();
-    mockUserRepository.get.mockResolvedValueOnce([testConstants.user1]);
+    mockUserRepository.get.mockResolvedValue([testConstants.user1]);
 });
 
 it('rejects non-encoded user id', async () => {
-    req.params = { user: testConstants.user1.id };
+    req.params = { user: '%E0%A4%A' };
 
     await getUser(req as Request<Params>, res as Response, next);
 
@@ -45,5 +47,22 @@ it('rejects non-matching user id', async () => {
 
     await getUser(req as Request<Params>, res as Response, next);
 
-    expect(next.mock.calls[0][0]).toMatchObject({ message: 'Not authorized', status: 403 });
+    const e = new NotAuthorizedException();
+    expect(next).toBeCalledWith(e);
+});
+
+it('returns 200 on user that does not exist', async () => {
+    mockUserRepository.get.mockResolvedValueOnce([]);
+
+    await getUser(req as Request<Params>, res as Response, next);
+
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith({ exists: false });
+});
+
+it('works on happy path', async () => {
+    await getUser(req as Request<Params>, res as Response, next);
+
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith({ exists: true });
 });
