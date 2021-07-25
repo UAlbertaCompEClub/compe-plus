33ce70661383fc229f7aa0b1301e57ef54fc54a3
rachel-ellis <rjellis@ualberta.ca>
2021-07-24 19:30:02 -0600

    style

--- www/src/components/auth/UserProfile.tsx
+++ www/src/components/auth/UserProfile.tsx
@@ -1,9 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import { IconButton, Menu, MenuItem, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { AccountCircle } from '@material-ui/icons';
 import React, { FC, useRef, useState } from 'react';
 
 const UserProfile: FC = () => {
+    const classes = useStyles();
     const userProfileIcon = useRef(null);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { logout } = useAuth0();
@@ -35,11 +37,32 @@ const UserProfile: FC = () => {
                 }}
                 open={isMenuOpen}
                 onClose={() => setIsMenuOpen(false)}
+                className={classes.menu}
             >
-                <MenuItem onClick={() => logout()}>Logout</MenuItem>
+                <Typography className={classes.menu_text}> Signed in as: [ccid] </Typography>
+                <MenuItem onClick={() => logout()} className={classes.menu_text} style={{ marginBottom: '100px' }}>
+                    Settings
+                </MenuItem>
+                <MenuItem onClick={() => logout()} className={classes.menu_text}>
+                    Log out
+                </MenuItem>
             </Menu>
         </>
     );
 };
+
+const useStyles = makeStyles((theme) => ({
+    menu: {
+        '& .MuiPaper-root': {
+            backgroundColor: theme.palette.primary.dark,
+            color: theme.palette.text.secondary,
+        },
+    },
+    menu_text: {
+        fontSize: 20,
+        fontWeight: 500,
+        padding: '15px',
+    },
+}));
 
 export default UserProfile;
