8e681283c31e13f3993cd32781f16b5eae3f74f2
Armianto Sumitro <armiantos@gmail.com>
2021-07-26 01:06:44 -0600

    Update App tests to mock redux hooks

--- www/src/App.test.tsx
+++ www/src/App.test.tsx
@@ -1,10 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { mocked } from 'ts-jest/utils';
 
 import App from './App';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { setupIntersectionObserverMock } from './util/test/intersectionObserverMock';
 import { withAuth0 } from './util/testWithAuth0';
 import { withRootState } from './util/testWithRootState';
+
+jest.mock('./redux/hooks');
 
 describe('App', () => {
     beforeEach(() => setupIntersectionObserverMock());
@@ -16,7 +20,18 @@ describe('App', () => {
         ${'interviewer'} | ${'🚧 Work in progress 🚧'} | ${'an interviewer'}
         ${'admin'}       | ${'🚧 Work in progress 🚧'} | ${'an admin'}
     `('renders correctly for $friendlyName', ({ role, containsString }) => {
-        render(withAuth0(withRootState(<App />, { user: { roles: [], currentRole: role, isLoading: false, hasRegistered: true } }), { isAuthenticated: true }));
+        const useAppDispatchMock = mocked(useAppDispatch, true);
+        const useAppSelectorMock = mocked(useAppSelector, true);
+
+        const dispatchMock = jest.fn();
+        useAppDispatchMock.mockReturnValue(dispatchMock);
+
+        const rootState = { user: { roles: [], currentRole: role, isLoading: false, hasRegistered: true } };
+        const auth0State = { isAuthenticated: true };
+
+        useAppSelectorMock.mockImplementation((selector) => selector(rootState));
+
+        render(withAuth0(withRootState(<App />, rootState), auth0State));
 
         const componentsWithText = screen.getAllByText(containsString);
         expect(componentsWithText[0]).toBeInTheDocument();
