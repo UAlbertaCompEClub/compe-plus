9bce865cee4b49a7e8922c7feb0a532eecd6f83b
Armianto Sumitro <armiantos@gmail.com>
2021-07-26 01:14:36 -0600

    Add check user registration test

--- www/src/App.test.tsx
+++ www/src/App.test.tsx
@@ -1,14 +1,23 @@
+import { AsyncThunkAction } from '@reduxjs/toolkit';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { mocked } from 'ts-jest/utils';
 
 import App from './App';
 import { useAppDispatch, useAppSelector } from './redux/hooks';
+import checkUserRegistration from './redux/thunks/checkUserRegistration';
+import { TokenAcquirer } from './util/auth0/fetchWithToken';
+import { User } from './util/serverResponses';
 import { setupIntersectionObserverMock } from './util/test/intersectionObserverMock';
 import { withAuth0 } from './util/testWithAuth0';
 import { withRootState } from './util/testWithRootState';
 
 jest.mock('./redux/hooks');
+const useAppDispatchMock = mocked(useAppDispatch, true);
+const useAppSelectorMock = mocked(useAppSelector, true);
+
+jest.mock('./redux/thunks/checkUserRegistration');
+const checkUserRegistrationMock = mocked(checkUserRegistration);
 
 describe('App', () => {
     beforeEach(() => setupIntersectionObserverMock());
@@ -20,9 +29,6 @@ describe('App', () => {
         ${'interviewer'} | ${'🚧 Work in progress 🚧'} | ${'an interviewer'}
         ${'admin'}       | ${'🚧 Work in progress 🚧'} | ${'an admin'}
     `('renders correctly for $friendlyName', ({ role, containsString }) => {
-        const useAppDispatchMock = mocked(useAppDispatch, true);
-        const useAppSelectorMock = mocked(useAppSelector, true);
-
         const dispatchMock = jest.fn();
         useAppDispatchMock.mockReturnValue(dispatchMock);
 
@@ -35,5 +41,22 @@ describe('App', () => {
 
         const componentsWithText = screen.getAllByText(containsString);
         expect(componentsWithText[0]).toBeInTheDocument();
+    });
+
+    it('calls check user registration on authenticated', () => {
+        const dispatchMock = jest.fn();
+        useAppDispatchMock.mockReturnValue(dispatchMock);
+
+        const rootState = { user: { roles: [], currentRole: '', isLoading: false, hasRegistered: null } };
+        const auth0State = { isAuthenticated: true };
+
+        const actionMock = {};
+        checkUserRegistrationMock.mockReturnValueOnce(actionMock as AsyncThunkAction<User | null | undefined, TokenAcquirer, never>);
+
+        useAppSelectorMock.mockImplementation((selector) => selector(rootState));
+
+        render(withAuth0(withRootState(<App />, rootState), auth0State));
+
+        expect(dispatchMock).toBeCalledWith(actionMock);
     });
 });
