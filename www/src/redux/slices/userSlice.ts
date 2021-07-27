4aecf785a4251b3c24e5a9dfaa3dc0846fe3db3e
armiantos <43416725+armiantos@users.noreply.github.com>
2021-07-26 23:03:26 -0600

    Redirect new users on authentication (#168)
    * Create endpoints file
    
    * Add helper to fetch with scopes
    
    * Rename fetchWithAuth0 to fetchWithScopes
    
    * Add userHasRegistered state
    
    * Copy over user reponse type from server
    
    * Fetch user registration status on log-in
    
    * Move thunk to different file
    
    * Update import
    
    * Mimic registration service
    
    * Delegate try catch to client
    
    * Add null value to userHasRegistered
    
    * Pass getAccessToken using function parameter
    
    * Update fetchWithScopes usage
    
    * Rename fetchWithScopes to fetchWithToken
    
    * Add register page
    
    * Redirect to register page if user doesn't exist
    
    * Install ts-jest
    
    * Refactor service call to be pure service call
    
    * Copy test constant from server
    
    * Add checkUserRegistration tests
    
    * Update App tests
    
    * Update App tests to mock redux hooks
    
    * Add check user registration test
    
    * Add not authenticated test for checkUserRegistration
    
    * Add redirection test
    
    * Update null being treated as false
    
    * Move `/register` to `UnauthenticatedApp`

--- www/src/redux/slices/userSlice.ts
+++ www/src/redux/slices/userSlice.ts
@@ -1,15 +1,37 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+import checkUserRegistration from '../thunks/checkUserRegistration';
+
+type UserState = {
+    roles: string[];
+    currentRole: string;
+    hasRegistered: boolean | null;
+    isLoading: boolean;
+};
+
+const initialState: UserState = {
+    roles: [],
+    currentRole: '',
+    hasRegistered: null,
+    isLoading: false,
+};
+
 export const userSlice = createSlice({
     name: 'user',
-    initialState: {
-        roles: [],
-        currentRole: '',
-    },
+    initialState,
     reducers: {
         setCurrentRole(state, action: PayloadAction<string>) {
             state.currentRole = action.payload;
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(checkUserRegistration.pending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(checkUserRegistration.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.hasRegistered = action.payload !== null;
+        });
     },
 });
 
---
+++ www/src/util/testConstants.ts
@@ -0,0 +1,17 @@
+import { User } from './serverResponses';
+
+const user1: User = {
+    id: 'google-oauth2|999937999992352499990',
+    email: 'email@domain.com',
+    ccid: 'myccid',
+    program: 'compe',
+    year: 4,
+    given_name: 'bob',
+    family_name: 'saggit',
+    full_name: 'bob saggit',
+    photo_url: 'asdf.com',
+    created_at: '2021-06-14T06:09:19.373404+00:00',
+    updated_at: '2021-06-14T06:09:19.373404+00:00',
+};
+
+export default { user1 };
