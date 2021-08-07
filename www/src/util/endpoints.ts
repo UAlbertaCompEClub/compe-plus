acff41b427e5e8564b272f13e9bdc399d5bd6be1
Armianto Sumitro <armiantos@gmail.com>
2021-08-07 17:08:57 -0600

    Add resume review endpoint

--- www/src/util/endpoints.ts
+++ www/src/util/endpoints.ts
@@ -2,3 +2,5 @@ import config from './config';
 
 export const me = `${config.server.endpoint}/api/v1/users/me`;
 export const users = `${config.server.endpoint}/api/v1/users`;
+
+export const resumeReviews = `${config.server.endpoint}/api/v1/resume-reviews`;
