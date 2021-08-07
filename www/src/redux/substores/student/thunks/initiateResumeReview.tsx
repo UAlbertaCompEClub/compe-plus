3d440402364159b8f99a5407f5befc019b847f61
Armianto Sumitro <armiantos@gmail.com>
2021-08-07 17:21:35 -0600

    Attach document along with resume review

--- www/src/redux/substores/student/thunks/initiateResumeReview.tsx
+++ www/src/redux/substores/student/thunks/initiateResumeReview.tsx
@@ -2,12 +2,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import postWithToken from '../../../../util/auth0/postWithToken';
 import TokenAcquirer from '../../../../util/auth0/TokenAcquirer';
-import { resumeReviews } from '../../../../util/endpoints';
+import { postDocuments, postResumeReviews } from '../../../../util/endpoints';
 import Scope from '../../../../util/scopes';
-import { ResumeReview } from '../../../../util/serverResponses';
+import { Document, ResumeReview } from '../../../../util/serverResponses';
 
 export type InitiateResumeReviewParams = {
-    reviewee: string;
+    userId: string;
+    base64Contents: string;
     tokenAcquirer: TokenAcquirer;
 };
 
@@ -15,12 +16,33 @@ type ResumeReviewBody = {
     reviewee: string;
 };
 
+type DocumentBody = {
+    note: string;
+    isReview: boolean;
+    userId: string;
+    base64Contents: string;
+};
+
 const initiateResumeReview = async (params: InitiateResumeReviewParams) => {
-    const res = await postWithToken<ResumeReviewBody, ResumeReview>(resumeReviews, params.tokenAcquirer, [Scope.CreateResumeReviews], {
-        reviewee: params.reviewee,
+    const resumeReviewResult = await postWithToken<ResumeReviewBody, ResumeReview>(postResumeReviews, params.tokenAcquirer, [Scope.CreateResumeReviews], {
+        reviewee: params.userId,
     });
 
-    return res;
+    if (resumeReviewResult?.data === undefined) {
+        // TODO: Reject with thunkApi
+    }
+
+    const documentResult = await postWithToken<DocumentBody, Document>(postDocuments(resumeReviewResult?.data.id ?? ''), params.tokenAcquirer, [Scope.CreateDocuments], {
+        note: '',
+        isReview: false,
+        userId: params.userId,
+        base64Contents: params.base64Contents,
+    });
+
+    return {
+        resumeReview: resumeReviewResult?.data,
+        document: documentResult?.data,
+    };
 };
 
 export default createAsyncThunk('resumeReview/initiate', initiateResumeReview);
