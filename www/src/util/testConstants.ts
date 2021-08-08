c6c90f34a6f4e32ef6be9ac2f18611e6d8059fa6
Armianto Sumitro <armiantos@gmail.com>
2021-08-07 18:44:11 -0600

    Add resume review test constant

--- www/src/util/testConstants.ts
+++ www/src/util/testConstants.ts
@@ -1,4 +1,4 @@
-import { User } from './serverResponses';
+import { ResumeReview, User } from './serverResponses';
 
 const user1: User = {
     id: 'google-oauth2|999937999992352499990',
@@ -14,4 +14,13 @@ const user1: User = {
     updated_at: '2021-06-14T06:09:19.373404+00:00',
 };
 
-export default { user1 };
+const resumeReview1: ResumeReview = {
+    id: '123e4567-e89b-12d3-a456-426614174000',
+    revieweeId: user1.id,
+    reviewerId: null,
+    state: 'seeking_reviewer',
+    createdAt: '2021-06-14T06:09:19.373404+00:00',
+    updatedAt: '2021-06-14T06:09:19.373404+00:00',
+};
+
+export default { user1, resumeReview1 };
