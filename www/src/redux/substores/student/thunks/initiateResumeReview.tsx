0c705cdf6238e6cff0ea31e695f65f96da925eed
Armianto Sumitro <armiantos@gmail.com>
2021-08-07 18:13:40 -0600

    Add thunk reject

--- www/src/redux/substores/student/thunks/initiateResumeReview.tsx
+++ www/src/redux/substores/student/thunks/initiateResumeReview.tsx
@@ -1,10 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk';
 
 import postWithToken from '../../../../util/auth0/postWithToken';
 import TokenAcquirer from '../../../../util/auth0/TokenAcquirer';
 import { postDocuments, postResumeReviews } from '../../../../util/endpoints';
 import Scope from '../../../../util/scopes';
 import { Document, ResumeReview } from '../../../../util/serverResponses';
+import { StudentDispatch, StudentState } from '../studentStore';
 
 export type InitiateResumeReviewParams = {
     userId: string;
@@ -23,13 +25,24 @@ type DocumentBody = {
     base64Contents: string;
 };
 
-const initiateResumeReview = async (params: InitiateResumeReviewParams) => {
+type AsyncThunkConfig = {
+    state: StudentState;
+    dispatch: StudentDispatch;
+    rejectValue: string;
+};
+
+type PayloadCreatorReturn = {
+    resumeReview?: ResumeReview;
+    document?: Document;
+};
+
+const initiateResumeReview = async (params: InitiateResumeReviewParams, thunkAPI: BaseThunkAPI<StudentState, unknown, StudentDispatch, string>) => {
     const resumeReviewResult = await postWithToken<ResumeReviewBody, ResumeReview>(postResumeReviews, params.tokenAcquirer, [Scope.CreateResumeReviews], {
         reviewee: params.userId,
     });
 
     if (resumeReviewResult?.data === undefined) {
-        // TODO: Reject with thunkApi
+        return thunkAPI.rejectWithValue('Unable to create resume review object');
     }
 
     const documentResult = await postWithToken<DocumentBody, Document>(postDocuments(resumeReviewResult?.data.id ?? ''), params.tokenAcquirer, [Scope.CreateDocuments], {
@@ -39,10 +52,14 @@ const initiateResumeReview = async (params: InitiateResumeReviewParams) => {
         base64Contents: params.base64Contents,
     });
 
+    if (documentResult?.data === undefined) {
+        return thunkAPI.rejectWithValue('Unable to upload document');
+    }
+
     return {
         resumeReview: resumeReviewResult?.data,
         document: documentResult?.data,
     };
 };
 
-export default createAsyncThunk('resumeReview/initiate', initiateResumeReview);
+export default createAsyncThunk<PayloadCreatorReturn, InitiateResumeReviewParams, AsyncThunkConfig>('resumeReview/initiate', initiateResumeReview);
