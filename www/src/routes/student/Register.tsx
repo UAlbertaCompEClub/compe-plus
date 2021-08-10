63ac2dd63c64e8658c8ccbcd067a28e332ef67de
Anushka Khare <khareanushka2015@gmail.com>
2021-08-09 21:09:35 -0600

    style registration page

--- www/src/routes/student/Register.tsx
+++ www/src/routes/student/Register.tsx
@@ -1,3 +1,4 @@
+import { Grid, Typography } from '@material-ui/core';
 import React, { FC, useState } from 'react';
 
 import StyledSelect from '../../components/Styledselect';
@@ -5,10 +6,22 @@ const Registration: FC = () => {
     const [year, setYear] = useState('');
     const [speciality, setSpeciality] = useState('');
     return (
-        <form>
-            <StyledSelect title='Year' value={year} onChange={setYear} choices={['1', '2', '3', '4', '5', '5+']} />
-            <StyledSelect title='Speciality' value={speciality} onChange={setSpeciality} choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad']} />
-        </form>
+        <div style={{ overflow: 'hidden' }}>
+            <Grid container justify='center' alignItems='center' spacing={8} style={{ marginTop: '10px', minHeight: '75vh' }}>
+                <Grid container item justify='center'>
+                    <Typography variant='h1'>Registration</Typography>
+                </Grid>
+
+                <Grid container item spacing={4}>
+                    <Grid container item xs={12} justify='center'>
+                        <form>
+                            <StyledSelect title='Year' value={year} onChange={setYear} choices={['1', '2', '3', '4', '5', '5+']} />
+                            <StyledSelect title='Speciality' value={speciality} onChange={setSpeciality} choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad']} />
+                        </form>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
     );
 };
 export default Registration;
