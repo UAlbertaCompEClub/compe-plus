43bb5746f126ea920d148211884c902bb4faca93
Anushka Khare <khareanushka2015@gmail.com>
2021-08-09 21:04:45 -0600

    cleaning up the registration page

--- www/src/components/Styledselect.tsx
+++ www/src/components/Styledselect.tsx
@@ -1,10 +1,11 @@
-import { InputLabel, MenuItem, Select } from '@material-ui/core';
+import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import React, { FC } from 'react';
-import { isPropertySignature } from 'typescript';
+
 type StyledSelectProps = { choices: string[]; title: string; value: string; onChange: (value: string) => void };
+
 const StyledSelect: FC<StyledSelectProps> = (props: StyledSelectProps) => {
     return (
-        <div>
+        <FormControl>
             <InputLabel id='demo-simple-select-label'>{props.title}</InputLabel>
 
             <Select labelId='demo-simple-select-label' id='demo-simple-select' value={props.value} onChange={(e) => props.onChange(e.target.value as string)}>
@@ -14,43 +15,8 @@ const StyledSelect: FC<StyledSelectProps> = (props: StyledSelectProps) => {
                     </MenuItem>
                 ))}
             </Select>
-        </div>
+        </FormControl>
     );
 };
-{
-    /* <Select 
-onChange = {(event) => props.handleChange(event.target.value)}
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-}));
-
-export default function MultilineTextFields() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState('Controlled');
-
-    const handleChange = (event: { target: { value: React.SetStateAction<string> } }) => {
-        setValue(event.target.value);
-    };
-
-    return (
-        <form className={classes.root} noValidate autoComplete='off'>
-            <div>
-                <TextField id='outlined-multiline-flexible' label='Year' multiline maxRows={4} value={value} onChange={handleChange} variant='outlined' />
-            </div>
-            <div>
-                <TextField id='outlined-multiline-flexible' label='Speciality' multiline maxRows={4} value={value} onChange={handleChange} variant='outlined' />
-            </div>
-            <select>
-              choices.map(el => <MenuItem value = {el.value}></MenuItem>
-            </select>
-        </form>
-    );
-} */
-}
 export default StyledSelect;
--- www/src/routes/student/Register.tsx
+++ www/src/routes/student/Register.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
-const Register: FC = () => {
-    return <div>Registration page</div>;
+import StyledSelect from '../../components/Styledselect';
+const Registration: FC = () => {
+    const [year, setYear] = useState('');
+    const [speciality, setSpeciality] = useState('');
+    return (
+        <form>
+            <StyledSelect title='Year' value={year} onChange={setYear} choices={['1', '2', '3', '4', '5', '5+']} />
+            <StyledSelect title='Speciality' value={speciality} onChange={setSpeciality} choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad']} />
+        </form>
+    );
 };
-
-export default Register;
+export default Registration;
