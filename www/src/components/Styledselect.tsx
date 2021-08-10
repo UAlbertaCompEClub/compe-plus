5b15c74ced40619d0cb307a1af7c22f7edd4fc1c
Anushka Khare <khareanushka2015@gmail.com>
2021-08-09 20:57:45 -0600

    add state handling for registration

--- www/src/components/Styledselect.tsx
+++ www/src/components/Styledselect.tsx
@@ -1,12 +1,13 @@
 import { InputLabel, MenuItem, Select } from '@material-ui/core';
 import React, { FC } from 'react';
-type StyledSelectProps = { choices: string[]; title: string };
+import { isPropertySignature } from 'typescript';
+type StyledSelectProps = { choices: string[]; title: string; value: string; onChange: (value: string) => void };
 const StyledSelect: FC<StyledSelectProps> = (props: StyledSelectProps) => {
     return (
         <div>
             <InputLabel id='demo-simple-select-label'>{props.title}</InputLabel>
 
-            <Select labelId='demo-simple-select-label' id='demo-simple-select'>
+            <Select labelId='demo-simple-select-label' id='demo-simple-select' value={props.value} onChange={(e) => props.onChange(e.target.value as string)}>
                 {props.choices.map((choice) => (
                     <MenuItem key={choice} value={choice}>
                         {choice}
--- www/src/routes/register/Registration.tsx
+++ www/src/routes/register/Registration.tsx
@@ -1,11 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import StyledSelect from '../../components/Styledselect';
 const Registration: FC = () => {
+    const [year, setYear] = useState('');
+    const [speciality, setSpeciality] = useState('');
     return (
         <form>
-            <StyledSelect title='Year' choices={['1', '2', '3', '4', '5', '5+']} />
-            <StyledSelect title='Speciality' choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad']} />
+            <StyledSelect title='Year' value={year} onChange={setYear} choices={['1', '2', '3', '4', '5', '5+']} />
+            <StyledSelect title='Speciality' value={speciality} onChange={setSpeciality} choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad']} />
         </form>
     );
 };
