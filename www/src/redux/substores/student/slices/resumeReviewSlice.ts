e5734bf6d7ca0c2e66a25b5a3f75f2af61a84c83
Armianto Sumitro <armiantos@gmail.com>
2021-08-12 15:57:48 -0600

    Integrate thunk to resumeReviewSlice

--- www/src/redux/substores/student/slices/resumeReviewSlice.ts
+++ www/src/redux/substores/student/slices/resumeReviewSlice.ts
@@ -1,12 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { ResumeReview } from '../../../../util/serverResponses';
+import getMyResumeReviews from '../thunks/getMyResumeReviews';
+
+type InitialState = {
+    resumeReviews: ResumeReview[];
+    currentResume: string | null; // base64encoded contents
+    isLoading: boolean;
+};
+
+const initialState: InitialState = {
+    resumeReviews: [],
+    currentResume: null,
+    isLoading: false,
+};
+
 export const resumeReviewSlice = createSlice({
     name: 'resumeReview',
-    initialState: {
-        resumes: [],
-        currentResume: null,
-    },
+    initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getMyResumeReviews.pending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(getMyResumeReviews.fulfilled, (state, action) => {
+            state.resumeReviews = action.payload.resumeReviews;
+        });
+    },
 });
 
 export default resumeReviewSlice.reducer;
