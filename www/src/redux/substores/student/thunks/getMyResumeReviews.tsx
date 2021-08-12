283b2fab5e492150c2d5768c9975d7f43211ee21
Armianto Sumitro <armiantos@gmail.com>
2021-08-12 16:03:01 -0600

    Fix getMyResumeReviews implementation

--- www/src/redux/substores/student/thunks/getMyResumeReviews.tsx
+++ www/src/redux/substores/student/thunks/getMyResumeReviews.tsx
@@ -1,15 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import postWithToken from '../../../../util/auth0/postWithToken';
+import fetchWithToken from '../../../../util/auth0/fetchWithToken';
 import TokenAcquirer from '../../../../util/auth0/TokenAcquirer';
-import { postResumeReviews } from '../../../../util/endpoints';
+import { getResumeReviews } from '../../../../util/endpoints';
 import Scope from '../../../../util/scopes';
 import { WrappedResumeReviews } from '../../../../util/serverResponses';
 import { StudentDispatch, StudentState } from '../studentStore';
 
 export type InitiateResumeReviewParams = {
-    userId: string;
-    base64Contents: string;
     tokenAcquirer: TokenAcquirer;
 };
 
@@ -20,9 +18,7 @@ type AsyncThunkConfig = {
 };
 
 export const getMyResumeReviews = async (params: InitiateResumeReviewParams): Promise<WrappedResumeReviews> => {
-    const resumeReviewResult = await postWithToken<unknown, WrappedResumeReviews>(postResumeReviews, params.tokenAcquirer, [Scope.ReadMyResumeReviews], {
-        reviewee: params.userId,
-    }).catch(() => {
+    const resumeReviewResult = await fetchWithToken<WrappedResumeReviews>(getResumeReviews, params.tokenAcquirer, [Scope.ReadMyResumeReviews]).catch(() => {
         throw new Error('Unable to fetch resume reviews');
     });
     return resumeReviewResult?.data ?? { resumeReviews: [] };
