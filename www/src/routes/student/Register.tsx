3605cbe5714da0fed2760bcf6f5e57589e67a6c6
Anushka Khare <khareanushka2015@gmail.com>
2021-08-12 20:10:08 -0600

    updated with changes from comments

--- www/src/routes/student/Register.tsx
+++ www/src/routes/student/Register.tsx
@@ -1,7 +1,8 @@
 import { Grid, Typography } from '@material-ui/core';
 import React, { FC, useState } from 'react';
 
-import StyledSelect from '../../components/Styledselect';
+import StyledSelect from '../../components/StyledSelect';
+
 const Registration: FC = () => {
     const [year, setYear] = useState('');
     const [speciality, setSpeciality] = useState('');
@@ -16,7 +17,12 @@ const Registration: FC = () => {
                     <Grid container item xs={12} justify='center'>
                         <form>
                             <StyledSelect title='Year' value={year} onChange={setYear} choices={['1', '2', '3', '4', '5', '5+']} />
-                            <StyledSelect title='Speciality' value={speciality} onChange={setSpeciality} choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad']} />
+                            <StyledSelect
+                                title='Speciality'
+                                value={speciality}
+                                onChange={setSpeciality}
+                                choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad', ' Other']}
+                            />
                         </form>
                     </Grid>
                 </Grid>
