ea78f0392cd61563eba6b96b94c7a4adce6f111e
Anushka Khare <khareanushka2015@gmail.com>
2021-08-12 20:14:35 -0600

    add app.tsx

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -13,6 +13,7 @@ import volunteerStore from './redux/substores/volunteeer/volunteerStore';
 import checkUserRegistration from './redux/thunks/checkUserRegistration';
 import MobileLanding from './routes/MobileLanding';
 import StudentApp from './routes/Student';
+import Register from './routes/student/Register';
 import UnauthenticatedApp from './routes/Unauthenticated';
 import theme from './styles/theme';
 import { COMMUNITY, COMMUNITY_ROUTE, COMPE_PLUS, MOCK_INTERVIEW, MOCK_INTERVIEW_ROUTE, REGISTER_ROUTE, RESUME_REVIEW, RESUME_REVIEW_ROUTE } from './util/constants';
@@ -77,6 +78,7 @@ const App: FC = () => {
                 <Router>
                     <Header sections={header_sections} title={COMPE_PLUS} />
                     <BrowserView renderWithFragment>
+                        <Register />
                         {content}
                         {!isLoadingUser && isUserRegistered === false && <Redirect to={REGISTER_ROUTE} />}
                     </BrowserView>
