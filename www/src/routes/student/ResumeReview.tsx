5134d4884d0c0419ba71095de87407509fb7504f
Armianto Sumitro <armiantos@gmail.com>
2021-08-16 13:37:36 -0600

    Add nested route for resumeReview

--- www/src/routes/student/ResumeReview.tsx
+++ www/src/routes/student/ResumeReview.tsx
@@ -1,17 +1,28 @@
 import { Grid, Typography } from '@material-ui/core';
 import React, { FC } from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import NoResumes from './resumeReview/NoResumes';
+import UploadResume from './resumeReview/UploadResume';
 
 const ResumeReview: FC = () => {
+    const { path, url } = useRouteMatch();
+
     return (
         <div style={{ overflow: 'hidden' }}>
             <Grid container justify='center' alignItems='center' spacing={8} style={{ marginTop: '10px', minHeight: '75vh' }}>
                 <Grid container item justify='center'>
                     <Typography variant='h1'>Resume Review</Typography>
                 </Grid>
+                <Switch>
+                    <Route exact path={path}>
+                        <NoResumes />
+                    </Route>
 
-                <NoResumes />
+                    <Route path={`${path}/upload`}>
+                        <UploadResume />
+                    </Route>
+                </Switch>
             </Grid>
         </div>
     );
