15504f4ae70147bd5bd2de1a3c4c7821a16c0699
Armianto Sumitro <armiantos@gmail.com>
2021-08-16 14:05:22 -0600

    Redirect to upload screen when button is clicked

--- www/src/routes/student/resumeReview/NoResumes.tsx
+++ www/src/routes/student/resumeReview/NoResumes.tsx
@@ -2,10 +2,14 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import UploadResumeIcon from '../../../assets/add_resume.svg';
+import { UPLOAD_RESUME_REVIEW_ROUTE } from '../../../util/constants';
 
 const NoResumes: React.FC = () => {
+    const history = useHistory();
+
     return (
         <Grid container item spacing={4}>
             <Grid container item xs={12} justify='center'>
@@ -15,7 +19,7 @@ const NoResumes: React.FC = () => {
                 <Typography>You have no resumes submitted</Typography>
             </Grid>
             <Grid container item xs={12} justify='center'>
-                <Button variant='contained' color='primary'>
+                <Button variant='contained' color='primary' onClick={() => history.push(UPLOAD_RESUME_REVIEW_ROUTE)}>
                     Submit resume
                 </Button>
             </Grid>
