10e32161f99716cdffcd1ef847347bf042f03563
Armianto Sumitro <armiantos@gmail.com>
2021-08-16 13:37:57 -0600

    Add pdf preview before submitting

--- www/src/routes/student/resumeReview/UploadResume.tsx
+++ www/src/routes/student/resumeReview/UploadResume.tsx
@@ -1,10 +1,11 @@
 import Grid from '@material-ui/core/Grid';
 import React, { FC, useEffect } from 'react';
 
+import PDFViewer from '../../../components/pdf/PDFViewer';
 import { resetUploadResume, setDocument } from '../../../redux/substores/student/slices/uploadResumeSlice';
-import { useStudentDispatch } from '../../../redux/substores/student/studentHooks';
+import { useStudentDispatch, useStudentSelector } from '../../../redux/substores/student/studentHooks';
 import { StudentDispatch } from '../../../redux/substores/student/studentStore';
-import { arrayBufferToBase64 } from '../../../util/helpers';
+import { arrayBufferToBase64, base64ToArrayBuffer } from '../../../util/helpers';
 
 const handleOnFileSelected = async (dispatch: StudentDispatch, files?: FileList | null) => {
     // TODO: Improve error handling
@@ -29,18 +30,30 @@ const handleOnFileSelected = async (dispatch: StudentDispatch, files?: FileList 
 const UploadResume: FC = () => {
     const dispatch = useStudentDispatch();
 
+    const { document } = useStudentSelector((state) => state.uploadResume);
+
     useEffect(() => {
         return () => {
             dispatch(resetUploadResume());
         };
     }, []);
 
-    return (
-        <Grid container item spacing={4}>
-            <Grid container justify='center' item xs={12}>
+    if (document === null) {
+        return (
+            <Grid container item xs={12} justify='center'>
                 {/* TODO: Replace with react-dropzone */}
                 <input type='file' aria-label='upload-resume' onChange={(e) => handleOnFileSelected(dispatch, e.target.files)} />
             </Grid>
+        );
+    }
+
+    const filePromise = async () => {
+        return base64ToArrayBuffer(document);
+    };
+
+    return (
+        <Grid container item xs={12}>
+            <PDFViewer fileName='string' filePromise={filePromise} />
         </Grid>
     );
 };
