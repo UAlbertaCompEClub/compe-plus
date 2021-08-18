bace4392eaf5e4b8dd28a6bedc4beaaf933759d2
Armianto Sumitro <armiantos@gmail.com>
2021-08-18 14:09:51 -0600

    Move form state to redux

--- www/src/routes/student/Register.tsx
+++ www/src/routes/student/Register.tsx
@@ -1,45 +1,57 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button, Grid, Typography } from '@material-ui/core';
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import StyledSelect from '../../components/StyledSelect';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { initializeUserInfo, setProgram, setYear } from '../../redux/slices/registerUserSlice';
+import checkUserRegistration from '../../redux/thunks/checkUserRegistration';
 import registerUser from '../../redux/thunks/registerUser';
 
 const Registration: FC = () => {
     const history = useHistory();
 
-    const [year, setYear] = useState('');
-    const [speciality, setSpeciality] = useState('');
-
     const { user, getAccessTokenSilently } = useAuth0();
 
-    const { hasRegistered } = useAppSelector((state) => state.user);
+    const { hasRegistered, info } = useAppSelector((state) => state.user);
     const dispatch = useAppDispatch();
 
     if (hasRegistered) {
         // Redirect back to home once register finishes
+        dispatch(checkUserRegistration(getAccessTokenSilently));
         history.replace('/');
     }
 
     const handleRegisterUser = () => {
+        if (info === null || info.year === undefined || info.program === undefined) {
+            alert('Form is incomplete, please complete the form');
+            return;
+        }
+
         dispatch(
             registerUser({
                 tokenAcquirer: getAccessTokenSilently,
-                userInfo: {
-                    ccid: user?.email?.split('@')[0] ?? '',
-                    email: user?.email ?? '',
-                    familyName: user?.family_name ?? '',
-                    fullName: user?.name ?? '',
-                    givenName: user?.given_name ?? '',
-                    id: user?.sub ?? '',
-                    program: speciality,
-                    year: parseInt(year),
-                },
+                userInfo: info,
             }),
         );
     };
+
+    useEffect(() => {
+        dispatch(
+            initializeUserInfo({
+                ccid: user?.email?.split('@')[0] ?? '',
+                email: user?.email ?? '',
+                familyName: user?.family_name ?? '',
+                fullName: user?.name ?? '',
+                givenName: user?.given_name ?? '',
+                id: user?.sub ?? '',
+            }),
+        );
+    }, []);
+
+    const yearChoices = ['1', '2', '3', '4', '5', '5+'];
+    const programChoices = ['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad', ' Other'];
 
     return (
         <div style={{ overflow: 'hidden' }}>
@@ -51,13 +63,8 @@ const Registration: FC = () => {
                 <Grid container item spacing={4}>
                     <Grid container item xs={12} justify='center'>
                         <form>
-                            <StyledSelect title='Year' value={year} onChange={setYear} choices={['1', '2', '3', '4', '5', '5+']} />
-                            <StyledSelect
-                                title='Speciality'
-                                value={speciality}
-                                onChange={setSpeciality}
-                                choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad', ' Other']}
-                            />
+                            <StyledSelect title='Year' value={info?.year.toString() ?? ''} onChange={(newYear) => dispatch(setYear(parseInt(newYear)))} choices={yearChoices} />
+                            <StyledSelect title='Speciality' value={info?.program ?? ''} onChange={(newProgram) => dispatch(setProgram(newProgram))} choices={programChoices} />
                             <Button onClick={handleRegisterUser}>Submit</Button>
                         </form>
                     </Grid>
