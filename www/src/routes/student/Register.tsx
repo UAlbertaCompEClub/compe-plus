3c306f21b9d9e9c775772594291557f991c5741e
Armianto Sumitro <armiantos@gmail.com>
2021-08-18 12:59:24 -0600

    Integrate register user call

--- www/src/routes/student/Register.tsx
+++ www/src/routes/student/Register.tsx
@@ -1,11 +1,46 @@
-import { Grid, Typography } from '@material-ui/core';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button, Grid, Typography } from '@material-ui/core';
 import React, { FC, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import StyledSelect from '../../components/StyledSelect';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import registerUser from '../../redux/thunks/registerUser';
 
 const Registration: FC = () => {
+    const history = useHistory();
+
     const [year, setYear] = useState('');
     const [speciality, setSpeciality] = useState('');
+
+    const { user, getAccessTokenSilently } = useAuth0();
+
+    const { hasRegistered } = useAppSelector((state) => state.user);
+    const dispatch = useAppDispatch();
+
+    if (hasRegistered) {
+        // Redirect back to home once register finishes
+        history.replace('/');
+    }
+
+    const handleRegisterUser = () => {
+        dispatch(
+            registerUser({
+                tokenAcquirer: getAccessTokenSilently,
+                userInfo: {
+                    ccid: user?.email?.split('@')[0] ?? '',
+                    email: user?.email ?? '',
+                    familyName: user?.family_name ?? '',
+                    fullName: user?.name ?? '',
+                    givenName: user?.given_name ?? '',
+                    id: user?.sub ?? '',
+                    program: speciality,
+                    year: parseInt(year),
+                },
+            }),
+        );
+    };
+
     return (
         <div style={{ overflow: 'hidden' }}>
             <Grid container justify='center' alignItems='center' spacing={8} style={{ marginTop: '10px', minHeight: '75vh' }}>
@@ -23,6 +58,7 @@ const Registration: FC = () => {
                                 onChange={setSpeciality}
                                 choices={['Computer Co-op', 'Software Co-op', 'Nano Co-op', 'Computer Trad', 'Nano Trad', ' Other']}
                             />
+                            <Button onClick={handleRegisterUser}>Submit</Button>
                         </form>
                     </Grid>
                 </Grid>
@@ -30,4 +66,5 @@ const Registration: FC = () => {
         </div>
     );
 };
+
 export default Registration;
