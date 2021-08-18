168bd4ebdf3cc7c05f73efbfb6dea287364bdbc4
Armianto Sumitro <armiantos@gmail.com>
2021-08-18 13:38:51 -0600

    Add redirect test for unauthenticated

---
+++ www/src/routes/Unauthenticated.test.tsx
@@ -0,0 +1,46 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { mocked } from 'ts-jest/utils';
+
+import { useAppSelector } from '../redux/hooks';
+import { RootState } from '../redux/store';
+import { REGISTER_ROUTE } from '../util/constants';
+import testConstants from '../util/testConstants';
+import UnauthenticatedApp from './Unauthenticated';
+
+jest.mock('../redux/hooks');
+const useAppSelectorMock = mocked(useAppSelector, true);
+
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: mockHistoryPush,
+    }),
+}));
+
+describe('Unauthenticated', () => {
+    let globalStoreMock: RootState;
+
+    beforeEach(() => {
+        globalStoreMock = testConstants.globalStoreMock;
+
+        useAppSelectorMock.mockImplementation((selector) => selector(globalStoreMock));
+    });
+
+    it('redirects to register page if the user has not registered', () => {
+        globalStoreMock.user.hasRegistered = false;
+
+        shallow(<UnauthenticatedApp />);
+
+        expect(mockHistoryPush).toBeCalledWith(REGISTER_ROUTE);
+    });
+
+    it('does not redirect if the user is registered', () => {
+        globalStoreMock.user.hasRegistered = true;
+
+        shallow(<UnauthenticatedApp />);
+
+        expect(mockHistoryPush).not.toBeCalledWith(REGISTER_ROUTE);
+    });
+});
