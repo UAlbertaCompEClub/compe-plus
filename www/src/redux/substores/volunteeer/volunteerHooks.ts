6889eaec542a322ff77f7268a2d8a0bf5ae13a9a
Armianto Sumitro <armiantos@gmail.com>
2021-08-19 12:43:00 -0600

    Rename volunteer hooks

--- www/src/redux/substores/volunteeer/volunteerHooks.ts
+++ www/src/redux/substores/volunteeer/volunteerHooks.ts
@@ -2,5 +2,5 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { VolunteerDispatch, VolunteerState } from './volunteerStore';
 
-export const useStudentDispatch = (): VolunteerDispatch => useDispatch<VolunteerDispatch>();
-export const useStudentSelector: TypedUseSelectorHook<VolunteerState> = useSelector;
+export const useVolunteerDispatch = (): VolunteerDispatch => useDispatch<VolunteerDispatch>();
+export const useVolunteerSelector: TypedUseSelectorHook<VolunteerState> = useSelector;
