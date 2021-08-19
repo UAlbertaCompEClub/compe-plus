850890d65f21f719b7c930fd8233be92aa42ed96
Armianto Sumitro <armiantos@gmail.com>
2021-08-19 12:49:59 -0600

    Add routes for volunteer

--- www/src/routes/Volunteer.tsx
+++ www/src/routes/Volunteer.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from 'react';
 import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 
 import volunteerStore from '../redux/substores/volunteeer/volunteerStore';
+import { COMMUNITY_ROUTE, MOCK_INTERVIEW_ROUTE, RESUME_REVIEW_ROUTE } from '../util/constants';
+import Landing from './unauthenticated/Landing';
 
 const VolunteerApp: FC = () => {
     return (
         <Provider store={volunteerStore}>
-            <p>🚧 Work in progress 🚧</p>
+            <Switch>
+                <Route path={RESUME_REVIEW_ROUTE}>
+                    <p>🚧 Work in progress 🚧</p>
+                </Route>
+                <Route path={MOCK_INTERVIEW_ROUTE}>
+                    <p>🚧 Work in progress 🚧</p>
+                </Route>
+                <Route path={COMMUNITY_ROUTE}>
+                    <p>🚧 Work in progress 🚧</p>
+                </Route>
+                <Route path='/'>
+                    <Landing />
+                </Route>
+            </Switch>
         </Provider>
     );
 };
