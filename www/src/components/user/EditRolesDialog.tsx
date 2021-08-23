0ffb7ccaa348064ceac0952b383be7ba1df191b4
Armianto Sumitro <armiantos@gmail.com>
2021-08-23 10:41:45 -0600

    Update status for switches

--- www/src/components/user/EditRolesDialog.tsx
+++ www/src/components/user/EditRolesDialog.tsx
@@ -1,6 +1,4 @@
-import { DialogContent } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
-import { DialogContentText } from '@material-ui/core';
+import { DialogContent, FormControlLabel, FormGroup } from '@material-ui/core';
 import { Switch } from '@material-ui/core';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -8,31 +6,57 @@ import React, { FC } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { closeEditRolesDialog, setCurrentRole } from '../../redux/slices/userSlice';
-import theme from '../../styles/theme';
 
 const EditRolesDialog: FC = () => {
     const dispatch = useAppDispatch();
 
-    const { isEditRolesDialogOpen, currentRole } = useAppSelector((state) => state.user);
-
-    const classes = useStyles();
+    const { isEditRolesDialogOpen, currentRole, roles } = useAppSelector((state) => state.user);
 
     return (
         <Dialog onClose={() => dispatch(closeEditRolesDialog())} open={isEditRolesDialogOpen}>
             <DialogTitle>Edit Roles</DialogTitle>
             <DialogContent>
-                <DialogContentText className={classes.dialog_content}>Resume Reviewer</DialogContentText>
+                <FormGroup row>
+                    <FormControlLabel
+                        labelPlacement='start'
+                        control={
+                            <Switch
+                                checked={currentRole === 'student'}
+                                onChange={() => dispatch(setCurrentRole('student'))}
+                                inputProps={{ 'aria-label': 'Toggle student role' }}
+                                disabled={!roles.includes('student')}
+                            />
+                        }
+                        label='Student'
+                    />
+                    <FormControlLabel
+                        labelPlacement='start'
+                        control={
+                            <Switch
+                                checked={currentRole === 'volunteer'}
+                                onChange={() => dispatch(setCurrentRole('volunteer'))}
+                                inputProps={{ 'aria-label': 'Toggle volunteer role' }}
+                                disabled={!roles.includes('reviewer' || !roles.includes('interviewer'))}
+                            />
+                        }
+                        label='Volunteer'
+                    />
+                    <FormControlLabel
+                        labelPlacement='start'
+                        control={
+                            <Switch
+                                checked={currentRole === 'admin'}
+                                onChange={() => dispatch(setCurrentRole('admin'))}
+                                inputProps={{ 'aria-label': 'Toggle admin role' }}
+                                disabled={!roles.includes('admin')}
+                            />
+                        }
+                        label='Admin'
+                    />
+                </FormGroup>
             </DialogContent>
-            <Switch checked={currentRole === 'student'} onChange={() => dispatch(setCurrentRole('student'))} inputProps={{ 'aria-label': 'Toggle student role' }} />
-            <DialogContent>
-                <DialogContentText className={classes.dialog_content}>Interviwer</DialogContentText>
-            </DialogContent>
-            <Switch checked={currentRole === 'volunteer'} onChange={() => dispatch(setCurrentRole('volunteer'))} inputProps={{ 'aria-label': 'Toggle volunteer role' }} />
         </Dialog>
     );
 };
-const useStyles = makeStyles(() => ({
-    dialog_content: { color: theme.palette.text.primary },
-}));
 
 export default EditRolesDialog;
