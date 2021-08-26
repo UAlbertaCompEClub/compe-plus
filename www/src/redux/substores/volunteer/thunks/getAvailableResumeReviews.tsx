e241f8a1f9e991f11df525cef41cf66444e9c412
Armianto Sumitro <armiantos@gmail.com>
2021-08-26 09:31:10 -0600

    Update response types

--- www/src/redux/substores/volunteer/thunks/getAvailableResumeReviews.tsx
+++ www/src/redux/substores/volunteer/thunks/getAvailableResumeReviews.tsx
@@ -4,7 +4,7 @@ import fetchWithToken from '../../../../util/auth0/fetchWithToken';
 import TokenAcquirer from '../../../../util/auth0/TokenAcquirer';
 import { getResumeReviews } from '../../../../util/endpoints';
 import Scope from '../../../../util/scopes';
-import { WrappedResumeReviews } from '../../../../util/serverResponses';
+import { WrappedResumeReviewWithNames } from '../../../../util/serverResponses';
 import { VolunteerDispatch, VolunteerState } from '../volunteerStore';
 
 export type GetAvailableResumeReviewParams = {
@@ -17,14 +17,14 @@ type AsyncThunkConfig = {
     rejectValue: string;
 };
 
-export const getAvailableResumeReviews = async (params: GetAvailableResumeReviewParams): Promise<WrappedResumeReviews> => {
-    const resumeReviewResult = await fetchWithToken<WrappedResumeReviews>(getResumeReviews, params.tokenAcquirer, [Scope.ReadAllResumeReviews], { state: 'seeking_reviewer' }).catch(() => {
+export const getAvailableResumeReviews = async (params: GetAvailableResumeReviewParams): Promise<WrappedResumeReviewWithNames> => {
+    const resumeReviewResult = await fetchWithToken<WrappedResumeReviewWithNames>(getResumeReviews, params.tokenAcquirer, [Scope.ReadAllResumeReviews], { state: 'seeking_reviewer' }).catch(() => {
         throw new Error('Unable to fetch available resume reviews');
     });
     return resumeReviewResult?.data ?? { resumeReviews: [] };
 };
 
-export default createAsyncThunk<WrappedResumeReviews, GetAvailableResumeReviewParams, AsyncThunkConfig>('resumeReview/getAvailableResumeReviews', (params, thunkApi) => {
+export default createAsyncThunk<WrappedResumeReviewWithNames, GetAvailableResumeReviewParams, AsyncThunkConfig>('resumeReview/getAvailableResumeReviews', (params, thunkApi) => {
     try {
         return getAvailableResumeReviews(params);
     } catch (e) {
