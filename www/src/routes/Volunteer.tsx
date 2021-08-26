bfc46d3db25c6901239e1e40807f1e77ba06ad31
Armianto Sumitro <armiantos@gmail.com>
2021-08-26 09:33:41 -0600

    Add editor route

--- www/src/routes/Volunteer.tsx
+++ www/src/routes/Volunteer.tsx
@@ -3,17 +3,21 @@ import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import volunteerStore from '../redux/substores/volunteer/volunteerStore';
-import { COMMUNITY_ROUTE, MOCK_INTERVIEW_ROUTE, RESUME_REVIEW_ROUTE } from '../util/constants';
+import { COMMUNITY_ROUTE, MOCK_INTERVIEW_ROUTE, RESUME_REVIEW_EDITOR_ROUTE, RESUME_REVIEW_ROUTE } from '../util/constants';
 import Landing from './unauthenticated/Landing';
 import MockInterview from './volunteer/MockInterview';
 import ResumeReview from './volunteer/ResumeReview';
+import ResumeReviewEditor from './volunteer/ResumeReviewEditor';
 
 const VolunteerApp: FC = () => {
     return (
         <Provider store={volunteerStore}>
             <Switch>
-                <Route path={RESUME_REVIEW_ROUTE}>
+                <Route exact path={RESUME_REVIEW_ROUTE}>
                     <ResumeReview />
+                </Route>
+                <Route path={RESUME_REVIEW_EDITOR_ROUTE}>
+                    <ResumeReviewEditor />
                 </Route>
                 <Route path={MOCK_INTERVIEW_ROUTE}>
                     <MockInterview />
--- www/src/util/constants.ts
+++ www/src/util/constants.ts
@@ -8,6 +8,7 @@ export const COMPE_CLUB = 'CompE Club';
 
 // Routes
 export const RESUME_REVIEW_ROUTE = '/resume-review';
+export const RESUME_REVIEW_EDITOR_ROUTE = '/resume-review/:resumeReviewId';
 export const MOCK_INTERVIEW_ROUTE = '/mock-interview';
 export const COMMUNITY_ROUTE = '/community';
 export const REGISTER_ROUTE = '/register';
