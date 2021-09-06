f98cface2b6d9f752baad5f601bf9fe9674f9103
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-09-05 22:54:19 -0600

    Make interviewees an array :(

--- api/db/migrations/003_add_calendly_resource.up.sql
+++ api/db/migrations/003_add_calendly_resource.up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS calendlys (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     link TEXT NOT NULL,
-    interviewee TEXT REFERENCES users ON DELETE RESTRICT,
+    interviewees TEXT[],
     interviewer TEXT NOT NULL REFERENCES users ON DELETE RESTRICT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
