86e25bfe5fa8de1c4ebc3e19e1a02ba13ea7c18c
Armianto Sumitro <armiantos@gmail.com>
2021-09-06 15:30:30 -0600

    Create resumeReview viewer component

---
+++ www/src/routes/student/resumeReview/ResumeReviewViewer.tsx
@@ -0,0 +1,77 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import LoadingOverlay from '../../../components/LoadingOverlay';
+import PDFViewer from '../../../components/pdf/PDFViewer';
+import { resetResumeReviewViewer } from '../../../redux/substores/student/slices/resumeReviewViewerSlice';
+import { useStudentDispatch, useStudentSelector } from '../../../redux/substores/student/studentHooks';
+import getMyDocuments from '../../../redux/substores/student/thunks/getMyDocuments';
+import { base64ToArrayBuffer } from '../../../util/helpers';
+
+type ResumeReviewParams = {
+    resumeReviewId: string;
+};
+
+const ResumeReviewEditor: React.FC = () => {
+    const classes = useStyles();
+
+    const { resumeReviewId } = useParams<ResumeReviewParams>();
+
+    const { currentDocument, isLoading } = useStudentSelector((state) => state.resumeReviewViewer);
+    const { getAccessTokenSilently } = useAuth0();
+    const dispatch = useStudentDispatch();
+
+    useEffect(() => {
+        dispatch(getMyDocuments({ tokenAcquirer: getAccessTokenSilently, resumeReviewId: resumeReviewId }));
+    }, [resumeReviewId]);
+
+    useEffect(() => {
+        return () => {
+            dispatch(resetResumeReviewViewer());
+        };
+    }, []);
+
+    const filePromise = async () => {
+        if (currentDocument === null) {
+            return new ArrayBuffer(0);
+        }
+        return base64ToArrayBuffer(currentDocument?.base64Contents);
+    };
+
+    return (
+        <Grid container>
+            <LoadingOverlay open={isLoading} />
+            <Grid item xs={8} className={classes.gridItem}>
+                <Typography>{resumeReviewId}</Typography>
+            </Grid>
+            <Grid container item xs={12}>
+                {currentDocument !== null && (
+                    <PDFViewer
+                        fileName={currentDocument.id}
+                        filePromise={filePromise}
+                        viewerConfig={{
+                            enableFormFilling: false,
+                            showAnnotationTools: false,
+                            showLeftHandPanel: false,
+                        }}
+                        className={classes.pdfContainer}
+                    />
+                )}
+            </Grid>
+        </Grid>
+    );
+};
+
+const useStyles = makeStyles((theme) => ({
+    gridItem: {
+        padding: theme.spacing(1),
+    },
+    pdfContainer: {
+        minHeight: '60vh',
+    },
+}));
+
+export default ResumeReviewEditor;
