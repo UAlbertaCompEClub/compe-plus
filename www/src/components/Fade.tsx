704f737a3388e0fec93fc759977a20bb7a8cda71
Armianto Sumitro <armiantos@gmail.com>
2021-09-06 22:13:46 -0600

    Move css to Fade.tsx

--- www/src/components/Fade.tsx
+++ www/src/components/Fade.tsx
@@ -1,8 +1,8 @@
 // Credit: dev.to/selbekk/how-to-fade-in-content-as-it-scrolls-into-view-10j4
 
-import '../styles/scrollAnimation.css';
-
 import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import React, { FC, useRef } from 'react';
 
 import useVisibility from '../hooks/useVisibility';
@@ -15,9 +15,25 @@ export const Fade: FC<FadeProps> = (props: FadeProps) => {
     const ref = useRef<HTMLDivElement>(null);
     const isVisible = useVisibility(ref);
 
+    const classes = useStyles();
+
     return (
-        <Grid container item justify='center' className={`fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={ref}>
+        <Grid container item justify='center' className={clsx(classes.fadeInSection, { [classes.isVisible]: isVisible })} ref={ref}>
             {props.children}
         </Grid>
     );
 };
+
+const useStyles = makeStyles({
+    fadeInSection: {
+        opacity: 0,
+        transform: 'translateY(20vh)',
+        visibility: 'hidden',
+        transition: 'opacity 0.6s cubic-bezier(0.65, 0, 0.35, 1), transform 1.2s cubic-bezier(0.65, 0, 0.35, 1)',
+    },
+    isVisible: {
+        opacity: 1,
+        transform: 'none',
+        visibility: 'visible',
+    },
+});
