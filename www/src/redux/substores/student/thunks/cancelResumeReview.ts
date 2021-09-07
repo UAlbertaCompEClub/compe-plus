055c253754d7258a0f2cddb331a3b255ace873e5
Armianto Sumitro <armiantos@gmail.com>
2021-09-06 20:28:56 -0600

    Add cancelResumeReview thunk

---
+++ www/src/redux/substores/student/thunks/cancelResumeReview.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import patchWithToken from '../../../../util/auth0/patchWithToken';
+import TokenAcquirer from '../../../../util/auth0/TokenAcquirer';
+import { patchMyResumeReview } from '../../../../util/endpoints';
+import Scope from '../../../../util/scopes';
+import { WrappedDocuments } from '../../../../util/serverResponses';
+import { StudentDispatch, StudentState } from '../studentStore';
+
+export type CancelResumeReviewParams = {
+    tokenAcquirer: TokenAcquirer;
+    resumeReviewId: string;
+};
+
+export type AsyncThunkConfig = {
+    state: StudentState;
+    dispatch: StudentDispatch;
+    rejectValue: string;
+};
+
+export const cancelMyResumeReview = async (params: CancelResumeReviewParams): Promise<void> => {
+    await patchWithToken<WrappedDocuments>(
+        patchMyResumeReview(params.resumeReviewId),
+        params.tokenAcquirer,
+        {
+            state: 'canceled',
+        },
+        [Scope.UpdateMyResumeReviews],
+    ).catch(() => {
+        alert('Unable to download resume');
+    });
+};
+
+export default createAsyncThunk<void, CancelResumeReviewParams, AsyncThunkConfig>('reviewResume/cancelResumeReview', (params, thunkApi) => {
+    try {
+        return cancelMyResumeReview(params);
+    } catch (e) {
+        return thunkApi.rejectWithValue(e);
+    }
+});
