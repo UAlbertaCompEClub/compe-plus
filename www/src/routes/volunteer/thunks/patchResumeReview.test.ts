87479f6b76774ebb581f0ed3042dea9545bc7edb
Armianto Sumitro <armiantos@gmail.com>
2021-09-11 12:15:20 -0600

    Fix tests

--- www/src/routes/volunteer/thunks/patchResumeReview.test.ts
+++ www/src/routes/volunteer/thunks/patchResumeReview.test.ts
@@ -14,76 +14,28 @@ const mockGetTokenSilently = jest.fn();
 
 it('works on happy path', async () => {
     mockPatchWithToken.mockResolvedValueOnce({} as AxiosResponse<void>);
-    mockPatchWithToken.mockResolvedValueOnce({} as AxiosResponse<void>);
 
     const result = await patchResumeReview({
         resumeReviewId: testConstants.document1.resumeReviewId,
         tokenAcquirer: mockGetTokenSilently,
-        documentId: testConstants.document1.id,
-        document: testConstants.document1.base64Contents,
         userId: testConstants.user1.id,
     });
 
-    expect(mockPatchWithToken).toBeCalledWith(
-        patchMyDocument(testConstants.document1.resumeReviewId, testConstants.document1.id),
-        mockGetTokenSilently,
-        {
-            note: '',
-            base64Contents: testConstants.document1.base64Contents,
-        },
-        [Scope.UpdateMyDocuments],
-    );
     expect(mockPatchWithToken).toBeCalledWith(patchMyResumeReview(testConstants.document1.resumeReviewId), mockGetTokenSilently, { state: 'finished' }, [Scope.UpdateMyResumeReviews]);
 
     expect(result).toBeUndefined();
 });
 
-it('throws an error if patch document fails', async () => {
-    mockPatchWithToken.mockRejectedValueOnce({});
-
-    await expect(
-        patchResumeReview({
-            resumeReviewId: testConstants.document1.resumeReviewId,
-            tokenAcquirer: mockGetTokenSilently,
-            documentId: testConstants.document1.id,
-            document: testConstants.document1.base64Contents,
-            userId: testConstants.user1.id,
-        }),
-    ).rejects.toThrow('Unable to patch document');
-
-    expect(mockPatchWithToken).toBeCalledWith(
-        patchMyDocument(testConstants.document1.resumeReviewId, testConstants.document1.id),
-        mockGetTokenSilently,
-        {
-            note: '',
-            base64Contents: testConstants.document1.base64Contents,
-        },
-        [Scope.UpdateMyDocuments],
-    );
-});
-
 it('throws an error if patch resume review fails', async () => {
-    mockPatchWithToken.mockResolvedValueOnce({} as AxiosResponse<void>);
     mockPatchWithToken.mockRejectedValueOnce({});
 
     await expect(
         patchResumeReview({
             resumeReviewId: testConstants.document1.resumeReviewId,
             tokenAcquirer: mockGetTokenSilently,
-            documentId: testConstants.document1.id,
-            document: testConstants.document1.base64Contents,
             userId: testConstants.user1.id,
         }),
     ).rejects.toThrow('Unable to mark resume review as finished');
 
-    expect(mockPatchWithToken).toBeCalledWith(
-        patchMyDocument(testConstants.document1.resumeReviewId, testConstants.document1.id),
-        mockGetTokenSilently,
-        {
-            note: '',
-            base64Contents: testConstants.document1.base64Contents,
-        },
-        [Scope.UpdateMyDocuments],
-    );
     expect(mockPatchWithToken).toBeCalledWith(patchMyResumeReview(testConstants.document1.resumeReviewId), mockGetTokenSilently, { state: 'finished' }, [Scope.UpdateMyResumeReviews]);
 });
