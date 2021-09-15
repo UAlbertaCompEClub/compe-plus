c2d9fafe5b0993354b19e32f83d7b3c53f3f27fa
Cyrus Diego <cyrus.d04@gmail.com>
2021-09-14 23:04:53 -0600

    fix lint

--- www/src/routes/student/resumeReview/ResumeList.tsx
+++ www/src/routes/student/resumeReview/ResumeList.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Box, Button, Grid, Typography } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import React, { FC, useEffect } from 'react';
