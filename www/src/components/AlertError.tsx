1da714e805f5cc3db6840987b875745c9e6664f7
Armianto Sumitro <armianto.sumitro@shopify.com>
2021-09-19 16:45:58 -0600

    Allow errorMessage to be null

--- www/src/components/AlertError.tsx
+++ www/src/components/AlertError.tsx
@@ -1,24 +1,31 @@
 import { IconButton, Snackbar } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 type AlertErrorProps = {
-    errorMessage: string;
+    errorMessage: string | null;
 };
 
 const AlertError: React.FC<AlertErrorProps> = ({ errorMessage }: AlertErrorProps) => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(errorMessage !== null);
 
     const handleClose = () => {
         setIsOpen(false);
     };
+
+    useEffect(() => {
+        if (errorMessage !== null) {
+            setIsOpen(true);
+        }
+    }, [errorMessage]);
+
     return (
         <Snackbar
             open={isOpen}
             autoHideDuration={6_000}
             onClose={() => setIsOpen(false)}
             message={errorMessage}
-            anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
             action={
                 <IconButton size='small' aria-label='close' color='inherit' onClick={handleClose}>
                     <CloseIcon fontSize='small' />
