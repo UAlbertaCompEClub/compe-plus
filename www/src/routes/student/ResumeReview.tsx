0dfd2ff523f3651b5426a5adec702745de38fdcb
Cyrus Diego <cyrus.d04@gmail.com>
2021-09-22 21:30:29 -0600

    small update to resume page  (#271)
    * small update
    
    * update
    
    * justifyyyy

--- www/src/routes/student/ResumeReview.tsx
+++ www/src/routes/student/ResumeReview.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import React, { FC } from 'react';
 
 import TitledPage from '../../components/TitledPage';
@@ -13,6 +13,13 @@ const ResumeReview: FC = () => {
         <TitledPage title='Resume Review'>
             <Grid container item justify='center'>
                 {isUploading ? <UploadResume /> : <ResumeList />}
+            </Grid>
+            <Grid container item justify='center'>
+                <Typography variant='body1' align='justify'>
+                    Please note that uploaded resumes should not be following the co-op format (if such a format exists). This platform is meant to provide resume reviews for students applying outside
+                    of PlacePro, and as such should be following &quot;industry standard&quot; resume formats. Please go to the Community page to see the relevant resources for building a resume prior
+                    to submitting a resume for review, thank you. 😁 (That being said, if you would like to have your co-op resume reviewed, feel free to upload it regardless 😎)
+                </Typography>
             </Grid>
         </TitledPage>
     );
