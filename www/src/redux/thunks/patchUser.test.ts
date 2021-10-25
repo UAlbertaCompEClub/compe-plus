1c2b0a97e8b3722cbdbf8691f50c02b26e5b2dfe
Armianto Sumitro <armiantos@gmail.com>
2021-10-25 00:52:21 -0600

    Fix tests

--- www/src/redux/thunks/patchUser.test.ts
+++ www/src/redux/thunks/patchUser.test.ts
@@ -1,21 +1,20 @@
+import { AxiosResponse } from 'axios';
 import { mocked } from 'ts-jest/utils';
 
 import patchWithToken from '../../util/auth0/patchWithToken';
 import { patchUser as patchUserEndpoint } from '../../util/endpoints';
 import Scope from '../../util/scopes';
 import testConstants from '../../util/testConstants';
-import patchUser from './patchUser';
+import { patchUser } from './patchUser';
 
 jest.mock('../../util/auth0/patchWithToken');
 const mockPatchWithToken = mocked(patchWithToken, true);
 
-let mockGetAccessTokenSilently: jest.MockedFunction<() => Promise<string>>;
-
-beforeEach(() => {
-    mockGetAccessTokenSilently = jest.fn();
-});
+const mockGetAccessTokenSilently = jest.fn();
 
 it('patches to the /users/:id endpoint', async () => {
+    mockPatchWithToken.mockResolvedValueOnce({} as AxiosResponse<void>);
+
     await patchUser({
         userId: testConstants.user1.id,
         tokenAcquirer: mockGetAccessTokenSilently,
@@ -41,5 +40,5 @@ it('throws an error when request fails', async () => {
             tokenAcquirer: mockGetAccessTokenSilently,
             hasAgreedToTermsOfService: true,
         }),
-    ).rejects.toThrow('Unable to fetch documents');
+    ).rejects.toThrow('Unable to update user details');
 });
