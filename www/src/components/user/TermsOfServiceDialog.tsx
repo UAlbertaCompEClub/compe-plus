b3cf572a86a9002ef7f0b7d200209bbc4c03917b
Armianto Sumitro <armiantos@gmail.com>
2021-10-25 23:10:34 -0600

    Remove unused import

--- www/src/components/user/TermsOfServiceDialog.tsx
+++ www/src/components/user/TermsOfServiceDialog.tsx
@@ -7,7 +7,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Divider from '@material-ui/core/Divider';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import { makeStyles } from '@material-ui/core/styles';
