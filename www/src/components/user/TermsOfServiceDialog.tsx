d48694280e4281f742d87bd4034d9065e81e4ae3
Armianto Sumitro <armiantos@gmail.com>
2021-10-26 18:36:20 -0600

    Replace asterisks with 'CompE+ and the Computer Engineering Club'

--- www/src/components/user/TermsOfServiceDialog.tsx
+++ www/src/components/user/TermsOfServiceDialog.tsx
@@ -52,20 +52,20 @@ const TermsOfServiceDialog: FC = () => {
                     <p>
                         <u>CONFIDENTIALITY NOT GUARANTEED</u> — Confidentiality of any submissions by you, including your curriculum vitae / resume, cannot be guaranteed. Internet transmissions are
                         vulnerable to interception, use, viruses and/or alteration by person(s) other than the intended recipient(s). Further, submissions may be subject to review by multiple persons
-                        involved with *.
+                        involved with CompE+ and the Computer Engineering Club.
                     </p>
                     <p>
-                        <u>NO REPRESENTATIONS</u> - INFORMATION ONLY, NOT ADVICE – Any feedback or information provided to you by * or its members in relation to career planning, interview preparation
-                        and curriculum vitae / resume review or otherwise is free of charge and merely the opinion of the person(s) providing such information. We caution that all such persons
-                        providing the information are students and none are required to have any special expertise, designations or credentials in this area. All such information is therefore provided
-                        without any representation or warranty, including without any representation of fitness for your purposes, and such information is not, and shall not be construed as,
-                        professional advice upon which you should rely.
+                        <u>NO REPRESENTATIONS</u> - INFORMATION ONLY, NOT ADVICE – Any feedback or information provided to you by CompE+ and the Computer Engineering Club or its members in relation to
+                        career planning, interview preparation and curriculum vitae / resume review or otherwise is free of charge and merely the opinion of the person(s) providing such information.
+                        We caution that all such persons providing the information are students and none are required to have any special expertise, designations or credentials in this area. All such
+                        information is therefore provided without any representation or warranty, including without any representation of fitness for your purposes, and such information is not, and
+                        shall not be construed as, professional advice upon which you should rely.
                     </p>
                     <p>
-                        <u>DISCLAIMER OF LIABILITY</u> – Accordingly, any communications or submissions between you, * and any of its members, and any reliance by you on any information provided to
-                        you as aforesaid, shall be done at your sole risk. None of *, nor its members, shall be liable for any damages or claims, whether direct, indirect, special, incidental,
-                        punitive or consequential, suffered by you or any other person relating to the information provided to you or the disclosure or interception of any transmissions, submissions
-                        or communications by or to you.
+                        <u>DISCLAIMER OF LIABILITY</u> – Accordingly, any communications or submissions between you, CompE+ and the Computer Engineering Club and any of its members, and any reliance
+                        by you on any information provided to you as aforesaid, shall be done at your sole risk. None of CompE+ and the Computer Engineering Club, nor its members, shall be liable for
+                        any damages or claims, whether direct, indirect, special, incidental, punitive or consequential, suffered by you or any other person relating to the information provided to you
+                        or the disclosure or interception of any transmissions, submissions or communications by or to you.
                     </p>
                 </DialogContentText>
                 <FormGroup row>
